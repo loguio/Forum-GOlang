2ab4a882dfee59487c3d4e72ff6bed136a8a6554
Marius_bourse <you@example.com>
2022-05-20 08:53:05 +0200

    autenthification

---
+++ db.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+type User struct {
+	Username string
+	Password string
+	Email    string
+}
+
+func signUp(user User) {
+	// SQLite is a file based database.
+	sqliteDatabase, _ := sql.Open("sqlite3", "./sqlite-database.db") // Open the created SQLite File
+	defer sqliteDatabase.Close()                                     // Defer Closing the database
+	// createTable(sqliteDatabase)                                      // Create Database Tables
+
+	// INSERT RECORDS
+	insertUser(sqliteDatabase, user.Username, user.Email, user.Password)
+
+	// DISPLAY INSERTED RECORDS
+	displayUser(sqliteDatabase)
+}
+
+func loginSQL(user User) {
+	sqliteDatabase, _ := sql.Open("sqlite3", "./sqlite-database.db") // Open the created SQLite File
+	defer sqliteDatabase.Close()                                     // Defer Closing the database
+	// SELECT RECORDS
+	ppl := User{}
+	row := sqliteDatabase.QueryRow("SELECT * FROM Customer WHERE UserName = ? AND password = ?", user.Username, user.Password)
+	// WHERE UserName = ? AND password = ?
+
+	row.Scan(&ppl.Username, &ppl.Email, &ppl.Password)
+	if ppl.Username == user.Username && ppl.Password == user.Password {
+		fmt.Println(ppl)
+		fmt.Println("vous êtes connecté avec succès")
+	} else {
+		fmt.Println("Ce compte n'existe pas")
+	}
+
+}
+
+// func createTable(db *sql.DB) {
+// 	createUserTableSQL := `CREATE TABLE IF NOT EXISTS Customer (
+// 		"UserName" TEXT NOT NULL PRIMARY KEY,
+// 		"Email" TEXT,
+// 		"password" TEXT
+// 	  );` // SQL Statement for Create Table
+
+// 	log.Println("Create Customer table...")
+// 	statement, err := db.Prepare(createUserTableSQL) // Prepare SQL Statement
+// 	if err != nil {
+// 		log.Fatal(err.Error())
+// 	}
+// 	statement.Exec() // Execute SQL Statements
+// 	log.Println("Users table created")
+// }
+
+// We are passing db reference connection from main to our method with other parameters
+func insertUser(db *sql.DB, UserName string, Email string, password string) {
+	log.Println("Inserting Users record ...")
+	insertUserSQL := `INSERT or IGNORE INTO Customer(UserName, Email, password) VALUES (?, ?, ?)`
+	statement, err := db.Prepare(insertUserSQL) // Prepare statement.
+	// This is good to avoid SQL injections
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	_, err = statement.Exec(UserName, Email, password)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+}
+
+func displayUser(db *sql.DB) {
+	row, err := db.Query("SELECT * FROM Customer ORDER BY UserName")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer row.Close()
+	for row.Next() { // Iterate and fetch the records from result cursor
+		var UserName string
+		var Email string
+		var password string
+		row.Scan(&UserName, &Email, &password)
+		log.Println("User: ", UserName, " ", Email, " ", password)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module main
+
+go 1.13
+
+require (
+	github.com/mattn/go-sqlite3 v2.0.3+incompatible
+	github.com/satori/go.uuid v1.2.0
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+)
